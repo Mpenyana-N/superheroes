14f3cdfcbc7755c06f486c154bfc45bca025cff2
Mpenyana-N <nickmpenyana@gmail.com>
2024-11-18 10:59:17 +0200

    Added CORS configurations

---
+++ src/main/java/com/mpenyana/superheroes/config/CorsConfig.java
@@ -0,0 +1,53 @@
+package com.mpenyana.superheroes.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfig.setAllowedHeaders(List.of("Origin",
+                "Access-Control-Allow-Origin",
+                "Content-Type",
+                "Accept",
+                "Authorization",
+                "Origin, Accept",
+                "X-Requested-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"
+        ));
+        corsConfig.setExposedHeaders(
+                Arrays.asList(
+                        "Origin",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials"
+                )
+        );
+        corsConfig.setAllowedMethods(
+                Arrays.asList("GET", "POST", "PUT", "DELETE",
+                        "OPTIONS")
+        );
+
+//        Registering our CORS configuration
+        var urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter();
+    }
+
+}

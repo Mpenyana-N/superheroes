778cc1e5214a25f25d6c411f8324dff1acd6d7b9
Mpenyana-N <nickmpenyana@gmail.com>
2024-11-19 06:42:03 +0200

    Added security policy

---
+++ src/main/java/com/mpenyana/superheroes/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.mpenyana.superheroes.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(form -> form
+                        .loginPage("/login")  // Custom login page (if any)
+                        .permitAll()          // Allow everyone to access the login page
+                );
+        return http.build();
+    }
+}
+
+
+
